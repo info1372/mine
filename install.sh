242b249b2019a3df8d18f813bc6ca7341a9cc053
info1372 <83560456+info1372@users.noreply.github.com>
2021-05-04 03:21:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt-get install git
 git clone https://github.com/info1372/mine.git
-sudo chmod 777 mine/mine.sh
-sudo chmod 777 mine/hellminer
-sudo chmod 777 mine/verus-solver
-sudo chmod 777 mine/install.sh
+sudo chmod 777 ~/mine/mine.sh
+sudo chmod 777 ~/mine/hellminer
+sudo chmod 777 ~/mine/verus-solver
+sudo chmod 777 ~/mine/install.sh
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
 echo "@reboot /"$USER"/mine/./mine.sh" >> mycron
