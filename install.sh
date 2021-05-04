7bb4f54e259a591b3982bffdd645986078b46a78
info1372 <83560456+info1372@users.noreply.github.com>
2021-05-04 23:17:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 nproc=$(nproc --all)
 sudo apt-get install git
 git clone https://github.com/info1372/mine.git
+cd mine
 sudo chmod 777 ~/mine/hellminer
 sudo chmod 777 ~/mine/mine.sh
 sudo chmod 777 ~/mine/verus-solver
