23f03820f1ef62fd32540d04c7319a934d58fc82
info1372 <83560456+info1372@users.noreply.github.com>
2021-05-04 20:49:18 +0100

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,2 @@
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu 4
+nproc=$(nproc --all)
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
