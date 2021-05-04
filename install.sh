142ea92d92ceac2d14897377d6d0153b8b8f948e
info1372 <83560456+info1372@users.noreply.github.com>
2021-05-04 03:13:10 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd mine
 sudo chmod 775 "$USER"/mine/mine.sh
 sudo chmod 775 "$USER"/mine/hellminer
 sudo chmod 775 "$USER"/mine/verus-solver
-sudo chmod 775 install.sh
+sudo chmod 775 "$USER"/mine/install.sh
 sudo touch /var/spool/cron/crontabs/"$USER"
 crontab -l > mycron
 echo "@reboot /"$USER"/mine/./mine.sh" >> mycron
